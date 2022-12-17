fba086717236af75ba75e50dea850df25299b406
hajaeryul <wofbf2@gmail.com>
2022-12-17 15:42:43 +0900

    22/12/17

---
+++ src/백준/반복문/bj15552.java
@@ -0,0 +1,32 @@
+package 백준.반복문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class bj15552 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int Num = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int i = 0; i < Num; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+
+	}
+
+}
