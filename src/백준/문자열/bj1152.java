7180bc09db51be741db0c8f8761868ec88684a91
hajaeryul <wofbf2@gmail.com>
2022-12-29 23:56:26 +0900

    5622 다이얼 왜 오류 ..?

---
+++ src/백준/문자열/bj1152.java
@@ -0,0 +1,20 @@
+package 백준.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj1152 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		System.out.println(st.countTokens());
+
+	}
+
+}
