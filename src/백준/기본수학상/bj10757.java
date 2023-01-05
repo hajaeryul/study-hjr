0859f1603765441d507b7db06335989adf871dce
hajaeryul <wofbf2@gmail.com>
2023-01-05 21:26:05 +0900

    23/01/05

---
+++ src/백준/기본수학상/bj10757.java
@@ -0,0 +1,26 @@
+package 백준.기본수학상;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class bj10757 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
+
+		A = A.add(B);
+		
+		System.out.println(A.toString());
+
+	}
+
+}
