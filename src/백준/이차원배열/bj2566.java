321889707ffc6df73734a5d9b08c93769a7957e8
hajaeryul <wofbf2@gmail.com>
2023-01-19 18:42:56 +0900

    23/01/19

---
+++ src/백준/이차원배열/bj2566.java
@@ -0,0 +1,36 @@
+package 백준.이차원배열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj2566 {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int A[][] = new int[9][9];
+		int max = 0;
+		int x = 0;
+		int y = 0;
+		
+		for(int i = 0; i < 9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < 9; j++) {
+				A[i][j] = Integer.parseInt(st.nextToken());
+				
+				if(A[i][j] > max) {
+					max = A[i][j];
+					x = i;
+					y = j;
+				}
+			}
+		}
+
+		System.out.println(max);
+		System.out.println((x+1) + " " + (y+1));
+	}
+
+}
