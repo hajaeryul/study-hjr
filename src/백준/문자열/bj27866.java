a63ab4853bdceccac47a426425b2c75cd5a3185f
hajaeryul <wofbf2@gmail.com>
2023-05-18 18:44:48 +0900

    23/05/18

---
+++ src/백준/문자열/bj27866.java
@@ -0,0 +1,19 @@
+package 백준.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj27866 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String s = br.readLine();
+		int i = Integer.parseInt(br.readLine());
+		
+		System.out.println(s.charAt(i-1));
+	}
+
+}
