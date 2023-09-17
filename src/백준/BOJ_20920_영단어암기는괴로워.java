3c5f0f60d40ff1d690b8d60c66eb2a058f8b68ef
hajaeryul <wofbf2@gmail.com>
2023-09-17 22:56:58 +0900

    BOJ 15787

--- src/백준/BOJ_20920_영단어암기는괴로워.java
+++ src/백준/BOJ_20920_영단어암기는괴로워.java
@@ -3,7 +3,7 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.ArrayList;	
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
