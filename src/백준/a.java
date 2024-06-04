2cd437209b497d1c9c372f12ddd9641c4a10aed8
하재률 <wofbf2@gmail.com>
2024-06-05 00:09:23 +0900

    algo

---
+++ src/백준/a.java
@@ -0,0 +1,12 @@
+package 백준;
+
+import java.util.Arrays;
+
+public class a {
+	public static void main(String[] args) {
+		String str = "  a  ";
+		String[] arr = str.split(" ", -1);
+		System.out.println(Arrays.toString(arr));
+	}
+	
+}
